88936a4316dc2a248b6e136f19e84fa3ecd75c17
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-08-22 10:40:28 +0200

    search tool created

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,4 +1,12 @@
 class Book < ApplicationRecord
   belongs_to :library
   has_many :bookings
+
+  def self.search(term)
+    if term
+      where('lower(title) LIKE ?', "%#{term}%").order('id DESC')
+    else
+      order('id DESC')
+    end
+  end
 end
