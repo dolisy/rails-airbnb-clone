c99530dc9c511547d7cff1c611a2b95d6e265c06
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-08-22 14:56:34 +0200

    validations + edit/update library done

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -1,4 +1,8 @@
 class Booking < ApplicationRecord
   belongs_to :user
   belongs_to :book
+
+  validates :book, presence: true, uniqueness: { scope: [ :user, :pick_up_date, :return_date ] }
+  validates :user, presence: true
+
 end
