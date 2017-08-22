0050194ed8ef54652e933a9d8e40d4327067a366
William Wei C <will30303@gmail.com>
2017-08-22 13:43:07 +0200

    conflict resolved

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-<<<<<<< HEAD
-ActiveRecord::Schema.define(version: 20170822095814) do
-=======
 ActiveRecord::Schema.define(version: 20170822093127) do
->>>>>>> master
-
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
