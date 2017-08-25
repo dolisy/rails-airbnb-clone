e0295566ec0bc246e2225d4b261572f4faf8f703
Albert Lee <yoonpoon@gmail.com>
2017-08-25 16:06:18 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -32,43 +32,43 @@ Library.destroy_all
 
 
 
-puts "creating 5 users..."
-(0..5).to_a.each do
-  user = User.new(email: Faker::Internet.email, password: Faker::Internet.password(8))
-  user.save!
-end
+# puts "creating 5 users..."
+# (0..5).to_a.each do
+#   user = User.new(email: Faker::Internet.email, password: Faker::Internet.password(8))
+#   user.save!
+# end
 
-puts "creating 20 libraries..."
-(0..20).to_a.each do
-  library = Library.new(name: Faker::ChuckNorris.fact, user: User.all.order('RANDOM()').first(), address: addresses.shuffle[0])
-  library.save!
-end
+# puts "creating 20 libraries..."
+# (0..20).to_a.each do
+#   library = Library.new(name: Faker::ChuckNorris.fact, user: User.all.order('RANDOM()').first(), address: addresses.shuffle[0])
+#   library.save!
+# end
 
-puts "creating 30 books..."
-(0..30).to_a.each do
-  book = Book.new(title: Faker::Book.title, genre: Faker::Book.genre, author: Faker::Book.author, publisher: Faker::Book.publisher, description: Faker::Lorem.paragraph, library: Library.all.order('RANDOM()').first())
-  book.address = book.library.address
-  book.save!
-end
-
-# puts "creating 30 books...with coordinates"
+# puts "creating 30 books..."
 # (0..30).to_a.each do
 #   book = Book.new(title: Faker::Book.title, genre: Faker::Book.genre, author: Faker::Book.author, publisher: Faker::Book.publisher, description: Faker::Lorem.paragraph, library: Library.all.order('RANDOM()').first())
 #   book.address = book.library.address
-#   # book.longitude = book.library.longitude
-#   # book.latitude = book.library.latitude
 #   book.save!
 # end
 
+# # puts "creating 30 books...with coordinates"
+# # (0..30).to_a.each do
+# #   book = Book.new(title: Faker::Book.title, genre: Faker::Book.genre, author: Faker::Book.author, publisher: Faker::Book.publisher, description: Faker::Lorem.paragraph, library: Library.all.order('RANDOM()').first())
+# #   book.address = book.library.address
+# #   # book.longitude = book.library.longitude
+# #   # book.latitude = book.library.latitude
+# #   book.save!
+# # end
 
-puts "creating 50 bookings..."
-(0..40).to_a.each do
-  booking = Booking.new(pick_up_date: Faker::Date.backward(14), return_date: Faker::Date.forward(23), user: User.all.order('RANDOM()').first(), book: Book.all.order('RANDOM()').first())
-  booking.save!
-end
 
-puts "creating 80 reviews..."
-(0..40).to_a.each do
-  review = Review.new(content: Faker::Lorem.paragraph, stars: Random.rand(5), user: User.all.order('RANDOM()').first(), booking: Booking.all.order('RANDOM()').first())
-  review.save!
-end
+# puts "creating 50 bookings..."
+# (0..40).to_a.each do
+#   booking = Booking.new(pick_up_date: Faker::Date.backward(14), return_date: Faker::Date.forward(23), user: User.all.order('RANDOM()').first(), book: Book.all.order('RANDOM()').first())
+#   booking.save!
+# end
+
+# puts "creating 80 reviews..."
+# (0..40).to_a.each do
+#   review = Review.new(content: Faker::Lorem.paragraph, stars: Random.rand(5), user: User.all.order('RANDOM()').first(), booking: Booking.all.order('RANDOM()').first())
+#   review.save!
+# end
