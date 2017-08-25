376350028061e3195579ca57500c631ff69944a4
Albert Lee <yoonpoon@gmail.com>
2017-08-25 16:32:40 +0200

    quick fix

--- app/models/book.rb
+++ app/models/book.rb
@@ -12,7 +12,7 @@ class Book < ApplicationRecord
   validates :library, presence: true
 
   geocoded_by :address
-  after_validation :geocode, if: :address_changed?
+  after_validation :geocode
 
   # for search
 
