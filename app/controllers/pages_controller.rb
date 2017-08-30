9dad0bef5361bdfef3ad4798e30be47bf3792fb4
Cyril Dolisy <dolisy.cyril@gmail.com>
2017-08-30 14:36:15 +0200

    will

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -33,7 +33,7 @@ class PagesController < ApplicationController
     #  options[:version] => "2013-08-01"
     #  options[:service] => "AWSECommerceService"
     Amazon::Ecs.configure do |options|
-      options[:AWS_access_key_id] = '[ENV['']]'
+      options[:AWS_access_key_id] = '[ENV['AWS_ACCESS_KEY_ID']]'
       options[:AWS_secret_key] = '[ENV['']]'
       options[:associate_tag] = '[ENV['']]'
     end
