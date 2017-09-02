d124d8906e1e0d972caf8a8f03f0573af315713d
Albert Lee <yoonpoon@gmail.com>
2017-09-02 13:51:15 +0200

    done

--- db/schema.rb
+++ db/schema.rb
@@ -49,11 +49,10 @@ ActiveRecord::Schema.define(version: 20170901131726) do
     t.date     "return_date"
     t.integer  "user_id"
     t.integer  "book_id"
-    t.datetime "created_at",          null: false
-    t.datetime "updated_at",          null: false
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
     t.date     "checkin_date"
     t.date     "checkout_date"
-    t.integer  "number_of_pick_days"
     t.string   "status"
     t.index ["book_id"], name: "index_bookings_on_book_id", using: :btree
     t.index ["user_id"], name: "index_bookings_on_user_id", using: :btree
